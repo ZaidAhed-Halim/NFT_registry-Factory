6708c1a83295dad98d03b50975792499944a0f37
zaid halim <zaid_abd.alhalim@yahoo.com>
2021-05-04 19:22:28 +0300

    initial commit

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,9 @@
+const NftRegistry = artifacts.require("NftRegistry");
+const CifiPowa = artifacts.require("CifiPowa");
+module.exports = async function (deployer) {
+  const accounts = await web3.eth.getAccounts();
+  const feeAccount = accounts[0];
+  console.log(feeAccount);
+  await deployer.deploy(NftRegistry,feeAccount);
+  await deployer.deploy(CifiPowa);
+};
